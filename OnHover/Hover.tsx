a8aa0b83813a3851aadbab15ee36acaab952c727
Allan Vazquez <jerichovazquez@gmail.com>
2023-05-10 11:34:48 +1000

    Implement React Lifecycle Overhaul

--- OnHover/Hover.tsx
+++ OnHover/Hover.tsx
@@ -4,24 +4,39 @@ export interface IHoverProps {
   label?: string,
   height?: string,
   width?: string,
-  initEvents: () => void
+  parentContainerId: string,
+  onMouseEnter: () => void,
+  onMouseLeave: () => void
 }
 
 export class Hover extends React.Component<IHoverProps> {
-  
+	private _element: HTMLDivElement | null = null;
+
 	componentDidMount(): void {
-		//wait 1 second before initializing events
-		setTimeout(()=>{
-			this.props.initEvents();
-		}, 1000);
+		const targetElement = this._element?.closest(`[data-control-name="${this.props.parentContainerId}"]`);
+		if (targetElement) {
+			targetElement.addEventListener("mouseenter", this.props.onMouseEnter);
+			targetElement.addEventListener("mouseleave", this.props.onMouseLeave);
+		}
 	}
-	public render(): React.ReactNode {
+
+	componentWillUnmount(): void {
+		const targetElement = this._element?.closest(`[data-control-name="${this.props.parentContainerId}"]`);
+		if (targetElement) {
+			targetElement.removeEventListener("mouseenter", this.props.onMouseEnter);
+			targetElement.removeEventListener("mouseleave", this.props.onMouseLeave);
+		}
+	}
+
+	render(): React.ReactNode {
+		const { label, height, width } = this.props;
 
 		return (
-		
-			<div style={{ height: this.props.height + "px", width: this.props.width+ "px" }}
+			<div
+				ref={(ref) => this._element = ref}
+				style={{ height: `${height}px`, width: `${width}px` }}
 			>
-				{this.props.label}
+				{label}
 			</div>
 		);
 	}
--- OnHover/index.ts
+++ OnHover/index.ts
@@ -1,19 +1,15 @@
-
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
 import { Hover, IHoverProps } from "./Hover";
 import * as React from "react";
 
 export class OnHover implements ComponentFramework.ReactControl<IInputs, IOutputs> {
-	private theComponent: ComponentFramework.ReactControl<IInputs, IOutputs>;
 	private _notifyOutputChanged: () => void;
-	private _isHovered:boolean;
-	private _isClicked:boolean;
-	private _mouseX:number;
-	private _mouseY:number;
-	private _isInsideContainer:boolean;
-	//private _rect: any;
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	constructor() { }
+	private _isHovered: boolean;
+
+	constructor() {
+		this._isHovered = false;
+	}
+
 	public init(
 		context: ComponentFramework.Context<IInputs>,
 		notifyOutputChanged: () => void,
@@ -21,83 +17,43 @@ export class OnHover implements ComponentFramework.ReactControl<IInputs, IOutput
 	): void {
 		this._notifyOutputChanged = notifyOutputChanged;
 		context.mode.trackContainerResize(true);
-		this._isHovered = false;
-		this._isClicked = false;
-		this._mouseX = 0;
-		this._mouseY = 0;
-    
 	}
+
 	public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement {
+		const width: number = parseInt(context.mode.allocatedWidth.toString());
+		const height: number = parseInt(context.mode.allocatedHeight.toString());
 
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		const width:number  = parseInt(context.mode.allocatedWidth.toString());
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		const height:number  = parseInt(context.mode.allocatedHeight.toString());
+		const handleMouseEnter = (): void => {
+			this._isHovered = true;
+			this._notifyOutputChanged();
+		};
 
-		//attach event listeners tom target element
-		const initEventListener = ():void => {
-
-			const targetElement = document.querySelector(`[data-control-name="${context.parameters.TargetControlId.raw}"]`);
-
-			if(targetElement != null){
-				
-				targetElement.addEventListener("mouseenter", () => {
-					this._isInsideContainer = true;
-				});
-				targetElement.addEventListener("mouseleave", () => {
-					this._isInsideContainer = false;
-					
-				});
-				document.addEventListener("mousemove", (e) => {
-					if(this._isInsideContainer && !this._isHovered){
-				
-						this._isHovered = true;
-					}
-					else if(!this._isInsideContainer && this._isHovered){
-			
-						this._isHovered = false;
-					}
-					this._mouseX = e.pageX;
-					this._mouseY = e.pageY;
-
-					this._notifyOutputChanged();
-				});
-
-				targetElement.addEventListener("mousedown", () => {
-					if(!this._isClicked){ 
-						this._isClicked = true;
-						this._notifyOutputChanged();
-					}
-				});
-				document.addEventListener("mouseup", () => {
-					if(this._isClicked){ 
-						this._isClicked = false;
-						this._notifyOutputChanged();
-					}
-				});
-			}
+		const handleMouseLeave = (): void => {
+			this._isHovered = false;
+			this._notifyOutputChanged();
 		};
 
 		const props: IHoverProps = { 
 			label: "", //for testing ,
 			height: height.toString(),
 			width: width.toString(),
-			initEvents: initEventListener
+			parentContainerId: context.parameters.ParentContainerId.raw!,
+			onMouseEnter: handleMouseEnter,
+			onMouseLeave: handleMouseLeave
 		};
 		return React.createElement(
 			Hover, props
 		);
+
+   
 	}
-	
+
 	public getOutputs(): IOutputs {
 		return { 
 			IsHovered: this._isHovered,
-			IsClicked: this._isClicked,
-			XCoordinate: this._mouseX,
-			YCoordinate: this._mouseY,
 		};
 	}
-	
+
 	public destroy(): void {
 		// Add code to cleanup control if necessary
 	}
